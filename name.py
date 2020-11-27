9166cab8d4059306b93b9fa0b9b210dec95275f8
Monkey-Young <Monkey_Young@outlook.com>
2020-11-27 11:12:21 +0800

    Add files via upload
    A python code "name.py"

---
+++ name.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+'''
+your name and age.
+'''
+name = input("your name: ")
+age = input("your age: ")
+
+#age = int(age) 或
+after = int(age) + 10
+
+print("your name is: ", name)
+print("after ten years, you are ", after)
